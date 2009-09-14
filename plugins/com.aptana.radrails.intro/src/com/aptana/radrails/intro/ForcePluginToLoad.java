01efc8b04179687a1a9fcdaceca0c3093d5ea439
Sandip Chitale <schitale@aptana.com>
2009-09-14 14:27:26 -0700

    Fix My Aptana and Release message locations.

--- plugins/com.aptana.radrails.intro/src/com/aptana/radrails/intro/ForcePluginToLoad.java
+++ plugins/com.aptana.radrails.intro/src/com/aptana/radrails/intro/ForcePluginToLoad.java
@@ -7,33 +7,31 @@ import org.osgi.service.prefs.BackingStoreException;
 
 import com.aptana.ide.documentation.DocumentationPlugin;
 import com.aptana.ide.intro.IntroPlugin;
+import com.aptana.ide.update.ui.UpdateUIActivator;
 import com.aptana.radrails.intro.editors.MyRadRailsEditor;
 
 public class ForcePluginToLoad implements IStartup
 {
 
 	private static final String RADRAILS_GETTING_STARTED_URL = "http://content.aptana.com/radrails/tutorials/index.php"; //$NON-NLS-1$
-	private static final String RADRAILS_REMOTE_IMAGE_LOCATION = "http://partners.aptana.com/adobe/content_ide/images/toolbar_intro.gif"; //$NON-NLS-1$
+	private static final String RADRAILS_REMOTE_IMAGE_LOCATION = "http://content.aptana.com/radrails/content_ide/images/toolbar_intro.gif"; //$NON-NLS-1$
 
 	// Custom release_message URL prefix
-	//	private static final String RADRAILS_RELEASE_MESSAGE_URL_PREFIX = "http://content.aptana.com/radrails/update/release-message/"; //$NON-NLS-1$
+	private static final String RADRAILS_RELEASE_MESSAGE_URL_PREFIX = "http://content.aptana.com/radrails/update/release-message/"; //$NON-NLS-1$
 
 	public void earlyStartup()
 	{
 		// FIXME Uncomment when we're building against studio in git!
-		// // Set up custom release_message URL prefix
-		// IEclipsePreferences updateUIPrefs = new InstanceScope()
-		// .getNode(UpdateUIActivator.PLUGIN_ID);
-		// updateUIPrefs
-		// .put(
-		// com.aptana.ide.update.preferences.IPreferenceConstants.RELEASE_MESSAGE_URL_PREFIX,
-		// RADRAILS_RELEASE_MESSAGE_URL_PREFIX);
-		// try {
-		// updateUIPrefs.flush();
-		// } catch (BackingStoreException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
+		// Set up custom release_message URL prefix
+		IEclipsePreferences updateUIPrefs = new InstanceScope().getNode(UpdateUIActivator.PLUGIN_ID);
+		updateUIPrefs.put(
+				com.aptana.ide.update.preferences.IPreferenceConstants.RELEASE_MESSAGE_URL_PREFIX,
+				RADRAILS_RELEASE_MESSAGE_URL_PREFIX);
+		try {
+			updateUIPrefs.flush();
+		} catch (BackingStoreException e) {
+			// TODO
+		}
 
 		// make My Adobe editor as the default intro editor
 		IEclipsePreferences introPluginPrefs = new InstanceScope().getNode(IntroPlugin.PLUGIN_ID);
