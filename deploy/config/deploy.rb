2c48c65848a4f4692fde674469886084983b9700
sgtcoolguy <cwilliams@aptana.com>
2009-09-23 14:51:34 -0700

    update deployment to push to new s3 location and folder structure, and to clean up existing releases before pushing, etc

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -1,9 +1,10 @@
 role :files, "10.10.1.21", :user => 'hudson'
 
 set :application, "rails" # Which project are we pushing (this controls folder name used from Hudson and s3)
-set :branch, "trunk" # which branch was it built on?
+set :branch, "master" # which branch was it built on?
 set :bucket_name, 'download.aptana.com' # Which bucket are we pushing it to?
-set :bucket_path_pref, "tools/radrails/plugin/update/#{application}"
+set :containing_folder_name, "#{application}"
+set :bucket_path_prefix, "tools/radrails/plugin/update"
 set :build_artifact_path, "/var/update-site/update/#{branch}" # Where does it live on the build file server?
 set :compressed_filename, "#{application}.tar.gz"
 
@@ -15,42 +16,68 @@ namespace :deploy do
   end
   
   desc "Download the compressed build artifacts"
-  task :grab, :depends => [:compress], :roles => :files do
+  task :grab, :roles => :files do
     get("/var/#{compressed_filename}", compressed_filename)
     sudo "rm -f /var/#{compressed_filename}"
   end
   
   desc "Uncompress the downloaded build artifacts"
-  task :uncompress, :depends => [:grab] do
+  task :uncompress do
     run_locally("tar -xvzf #{compressed_filename}")
     run_locally("rm -f #{compressed_filename}")
   end
   
-  desc "Extract the release version number and change the containing directory name to it"
-  task :rename, :depends => [:uncompress] do
-    value = Dir.glob("#{application}/*-*.zip")
-    set :release_version, value[0].match(/.+(\d+\.\d+\.\d+\.\d+).+/)[1]
-    FileUtils.mv(application, release_version)
+  # Used in our "legacy" update scheme where we assume unique version numbers to releases and then do apache redirects
+  #  desc "Extract versio number and set up so we store under the version name as folder in S3"
+  #  task :extract_version, :depends => [:uncompress] do
+  #    value = Dir.glob("#{application}/*-*.zip")
+  #    set :containing_folder_name, value[0].match(/.+(\d+\.\d+\.\d+\.\d+).+/)[1]
+  #    set :bucket_path_prefix, "#{bucket_path_prefix}/#{application}"
+  #  end
+  
+  desc "Change the directory name to what we'll use on S3"
+  task :rename do
+    FileUtils.mv(application, containing_folder_name) if application != containing_folder_name
+  end
+  
+  desc "Connects to S3 using our credentials"
+  task :connect_s3 do 
+    require 'aws/s3'
+    require 'yaml'
+    AWS::S3::Base.establish_connection!(YAML.load_file('config/s3.yml'))   
+  end
+  
+  desc "Cleans any existing release on s3 before pushing over top"
+  task :clean_s3 do
+    objects = []
+    # Apparently this doesn't return everything, so we have to keep looping until empty
+    begin
+      objects = AWS::S3::Bucket.objects(bucket_name, :prefix => "#{bucket_path_prefix}/#{containing_folder_name}")
+      objects.each do |object|
+        puts "Removing old #{object.key}"
+        object.delete
+      end
+    end while !objects.empty?
   end
   
   desc "Push the build artifacts up to S3"
-  task :push, :depends => [:rename] do
-    require 'aws/s3'
-    require 'yaml'
-    AWS::S3::Base.establish_connection!(YAML.load_file('config/s3.yml'))    
-    files = File.join(release_version, "**", "*")
+  task :push do 
+    files = File.join(containing_folder_name, "**", "*")
     Dir.glob(files).each do |filename|
       next if File.directory?(filename)
       puts "Pushing #{filename}..."
-      remote_path = "#{bucket_path_pref}/" + filename
+      remote_path = "#{bucket_path_prefix}/" + filename
       AWS::S3::S3Object.store(remote_path, open(filename), bucket_name, :access => :public_read)
     end
   end
   
   desc "Cleans up the extract build artifacts locally"
   task :clean do
-    run_locally("rm -rf #{application}")
+    run_locally("rm -rf #{containing_folder_name}")
   end
+  
+  before "deploy:clean_s3", "deploy:connect_s3"
+  before "deploy:push", "deploy:connect_s3", "deploy:clean_s3"
   
   desc "Run all the necessary deploy tasks in the correct order"
   task :default do
